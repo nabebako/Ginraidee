fa9e56442beff3e6a4e55dc7e9115dc7ff9e87cc
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-02 16:14:09 +0700

    update searchbar

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,10 @@ var search_qurey = '';
 
 
 window.onload = function() { /*Setting up event listeners*/
-    var search_bar = document.getElementById('Search-Query');
-    document.querySelector('input').addEventListener('input',() => {});
-    search_bar.addEventListener('input', (qurey) => { 
-        search_qurey = qurey.target.value;
-        console.log(search_qurey);
-    });
+    document.getElementById('Search-Query')
+            .addEventListener('input', (qurey) => {
+                search_qurey = qurey.target.value;
+            });
 }
 
 
