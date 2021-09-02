5d70ef7c205f553849a78fa3ede2b4c9155ea765
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-02 16:11:56 +0700

    Create a searchbar

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,20 @@
-var i = 12;
-//Ken
+var search_qurey = '';
+
+
+window.onload = function() { /*Setting up event listeners*/
+    var search_bar = document.getElementById('Search-Query');
+    document.querySelector('input').addEventListener('input',() => {});
+    search_bar.addEventListener('input', (qurey) => { 
+        search_qurey = qurey.target.value;
+        console.log(search_qurey);
+    });
+}
+
+
+
+
+/*
+document.querySelector('input').addEventListener('input', (x) => {
+    console.log(x);
+})
+*/
