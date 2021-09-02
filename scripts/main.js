258409a49631544fadf40aa57f7053fcfe9463f6
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-02 16:25:41 +0700

    Add event listener
    For the questionnaire popup.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,14 @@
 var search_qurey = '';
 
-
 window.onload = function() { /*Setting up event listeners*/
     document.getElementById('Search-Query')
             .addEventListener('input', (qurey) => {
                 search_qurey = qurey.target.value;
+                console.log('query: ' + search_qurey);
+            });
+    document.getElementById('Questionnaire-pop-up')
+            .addEventListener('click', () => {
+                console.log('create a window');
             });
 }
 
