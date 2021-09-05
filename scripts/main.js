2877646d7fa8090ab96c82b5d4855a3ea9ae0dcb
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-05 23:06:39 +0700

    Cleanup

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,13 @@ var search_qurey = '';
 window.onload = function() { /*Setting up event listeners*/
     document.getElementById('Search-Query')
             .addEventListener('input', (qurey) => {
-                search_qurey = qurey.target.value;
+                search_qurey = qurey.target.value.replaceAll(/[^0-9a-z ]/ig, '');
                 console.log('query: ' + search_qurey);
             });
     document.getElementById('Questionnaire-pop-up')
             .addEventListener('click', () => {
+                /*Create a poup window.*/
+
                 console.log('create a window');
             });
 }
