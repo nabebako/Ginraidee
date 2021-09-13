d2642d784fcae2f04e0d509bb7a0f86378304bb5
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-13 09:22:33 +0700

    Try a fade in/out function

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,32 @@
 var search_qurey = '';
 let hamburger_nav_is_open = false;
 
-function fade_in(element) {
-    var op = 1;
-    
+function fade_in(element, max_op) {
+    element.style.classList.remove('hidden');
+    var op = 0.1;  // initial opacity
+    var timer = setInterval(() => {
+        if (op >= max_op) {
+            clearInterval(timer);
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op += op * 0.1;
+    }, 10);
 }
 
 function fade_out(element) {
-
+    var op = element.style.opacity;
+    var timer = setInterval(() => {
+        if (op <= 0.1) {
+            clearInterval(timer);
+            element.style.classList.add('hidden');
+        }
+        element.style.opacity = op;
+        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
+        op -= op * 0.1;
+    }, 10);
 }
+
 
 
 window.onload = function() { /*Setting up event listeners*/
@@ -18,16 +36,17 @@ window.onload = function() { /*Setting up event listeners*/
     });
 
     document.getElementById('Questionnaire-pop-up').addEventListener('click', () => {
-        /*Create a poup window.*/
+        /*Create a pop-up window.*/
         document.body.classList.add('stop--scroll'); /*Stops scrolling*/
-        document.getElementById('pop-up--foreground').style.display = 'block';
-        document.getElementById('pop-up--background').style.display = 'block';
+        fade_in(document.getElementById('pop-up--foreground'), 0.3);
+        fade_in(document.getElementById('pop-up--background'), 1);
         console.log('Window Created');
     });
 
     document.getElementById('window--close').addEventListener('click', () => {
-        document.getElementById('pop-up--foreground').style.display = 'none';
-        document.getElementById('pop-up--background').style.display = 'none';
+        /*Get rid of the pop-up window*/
+        fade_out(document.getElementById('pop-up--foreground'));
+        fade_out(document.getElementById('pop-up--background'));
         document.body.classList.remove('stop--scroll');
         console.log('Windown Destroyed');
     });
