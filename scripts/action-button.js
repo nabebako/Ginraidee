aa2c668b2d4f046e58866f849ac8ed229783710f
this-is-just-typical <natchanon.ken135@gmail.com>
2021-09-30 12:31:47 +0700

    added function to the action button.

---
+++ scripts/action-button.js
@@ -0,0 +1,5 @@
+document.getElementById('add_to_list').addEventListener('click', () => {}); // Add the page id to list
+
+document.getElementById('download').addEventListener('click', () => {}); // Dowload the page in print format
+
+document.getElementById('print').addEventListener('click', () => {}); // Print the page
