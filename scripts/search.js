f7fee01f0843b3eb4120151c6697512def7629cc
this-is-just-typical <natchanon.ken135@gmail.com>
2021-10-04 22:42:31 +0700

    added url query reader.

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,27 @@
 var search_qurey = '';
+var query_result = [];
 
 const SQ = document.getElementById('search-query');
 const SQ_results = document.getElementById('search-result');
 
+function query_database() {
+    console.log('query: ' + search_qurey);
+    return [];
+}
+
+
+window.onload = () => { 
+    // get the query from the url
+    search_qurey = document.URL.split('?')[1].split('&').filter(query => /query/.test(query))[0].split('=')[1].replace('-',' ');
+
+    query_result = query_database();
+}
+
+
 SQ.addEventListener('input', (qurey) => {
     search_qurey = qurey.target.value.replaceAll(/[^0-9a-z ]/ig, '');
-    console.log('query: ' + search_qurey);
+    
+    query_result = query_database();
 });
 
 // For passing query to another page, when user press enter //
@@ -13,10 +29,10 @@ SQ.addEventListener('keypress', (event) => {
     if(event.key === 'Enter') {
         search_qurey = search_qurey.replace(/\s/g,'-');
         if(/index.html/.test(window.location.href)) {
-            window.location.assign('./pages/shopping-list.html?query=' + search_qurey); 
+            window.location.assign('./pages/search.html?query=' + search_qurey); 
         } 
         else {
-            window.location.assign('./shopping-list.html?query=' + search_qurey);
+            window.location.assign('./search.html?query=' + search_qurey);
         }
     }
 });
