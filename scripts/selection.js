57475fcb6cc61807c96120686111f6ec4a232c27
this-is-just-typical <natchanon.ken135@gmail.com>
2021-10-06 15:41:48 +0700

    I'll deal with this later

--- scripts/selection.js
+++ scripts/selection.js
@@ -4,11 +4,13 @@ const client = new Client({
     host: 'localhost',
     user: 'postgres',
     port: 5432,
-    password: 'postgres',
-    database: 'postgres'
+    password: 'blue_tea',
+    database: 'main'
 });
 
 client.connect()
 .then(() => console.log('connected'))
+.then(() => client.query("SELECT * FROM test"))
+.then(result => console.table(result.rows))
 .catch(e => console.log(e))
 .finally(() => client.end());
